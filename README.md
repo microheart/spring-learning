94b7b2e4ec5ffef97495ac13cac4e92a1777ba36
xushun <xsshun@gmail.com>
2017-09-28 18:00:05 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring Learning
+
+Spring 核心技术学习，验证和思考
+
---
+++ src/main/java/com/iknowers/learning/spring/Application.java
@@ -0,0 +1,15 @@
+package com.iknowers.learning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xushun
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
